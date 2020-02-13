d7bd3fff474cb8ffb75065e51ce2a6488cbfc96f
Kevin Nentwig <40609589+KevinNentwig@users.noreply.github.com>
2020-02-13 10:41:19 +0100

    added combat with weapon range mechanic

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+        
+        Transform target;
+
+        Mover mover;
+
+        private void Start() 
+        {
+            mover = GetComponent<Mover>();
+        }
+
+        private void Update()
+        {
+            if (target == null) return;
+            if (!GetIsInRange())
+            {
+                mover.MoveTo(target.position);
+            }
+            else
+            {
+                mover.Cancel();
+            }
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget.CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;
+        }
+
+
+        public void Cancel()
+        {
+            target = null;
+        }
+    }
+}
