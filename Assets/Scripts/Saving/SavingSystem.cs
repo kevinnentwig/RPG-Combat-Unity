773a486566330ecbd68171ab5dfbb1f1f1539e65
Kevin Nentwig <40609589+KevinNentwig@users.noreply.github.com>
2020-02-21 16:01:18 +0100

    added saving system, currently set for plyr pos

---
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingSystem : MonoBehaviour
+    {
+        public void Save(string saveFile)
+        {
+            string path = GetPathFromSaveFile(saveFile);
+            print("Saving to " + path);
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                Transform playerTransform = GetPlayerTransform();                
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 position = new SerializableVector3(playerTransform.position);
+                formatter.Serialize(stream, position);
+            }
+        }
+
+        public void Load(string saveFile)
+        {
+            string path = GetPathFromSaveFile(saveFile);
+            print("Loading from " + saveFile);
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                Transform playerTransform = GetPlayerTransform();
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 position = (SerializableVector3)formatter.Deserialize(stream);
+                playerTransform.position = position.ToVector();
+            }
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
+        private string GetPathFromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
+        }
+    }
+}
