107d1abbd2af0f34aba43af8d528a9b363382603
Kevin Nentwig <40609589+KevinNentwig@users.noreply.github.com>
2020-03-01 16:01:11 +0100

    added experience points and display

---
+++ Assets/Scripts/Stats/Experience.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.Stats
+{
+    public class Experience : MonoBehaviour, ISaveable
+    {
+        [SerializeField] float experiencePoints = 0f;
+
+        public void GainExperience(float experience)
+        {
+            experiencePoints += experience;
+        }
+
+        public float GetPoints()
+        {
+            return experiencePoints;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
+        }
+
+    }
+}
