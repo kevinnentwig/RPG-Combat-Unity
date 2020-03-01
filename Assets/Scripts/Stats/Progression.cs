be9bdda5a2454e24d2f96636c986b75a19253e7f
Kevin Nentwig <40609589+KevinNentwig@users.noreply.github.com>
2020-03-01 15:18:10 +0100

    optimized GetStat BuildLookup

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace RPG.Stats
 {
@@ -7,21 +8,38 @@ namespace RPG.Stats
     {
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
+            BuildLookup();
+            float[] levels = lookupTable[characterClass][stat];
+
+            if  (levels.Length < level)
+            {
+                return 0;
+            }
+
+            return levels[level - 1];
+        }
+
+        private void BuildLookup()
+        {
+            if (lookupTable != null) return;
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+            
             foreach (ProgressionCharacterClass progressionClass in characterClasses)
             {
-                if (progressionClass.characterClass != characterClass) continue;
-                
+                var statLookUpTable = new Dictionary<Stat, float[]>();
+
                 foreach (ProgressionStat progressionStat in progressionClass.stats)
                 {
-                    if (progressionStat.stat != stat) continue;
+                    statLookUpTable[progressionStat.stat] = progressionStat.levels;
+                }
 
-                    if (progressionStat.levels.Length < level) continue;
-                    return progressionStat.levels[level - 1];
-                }   
+                lookupTable[progressionClass.characterClass] = statLookUpTable;
             }
-            return 0;
         }
 
         [System.Serializable]
