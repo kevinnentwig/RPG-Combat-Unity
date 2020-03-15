2a4f1697cac7e8fdd3ad54d4fcf6a7b7afbc7833
Kevin Nentwig <40609589+KevinNentwig@users.noreply.github.com>
2020-03-15 14:52:46 +0100

    fixed healthbar rotation bug

--- Assets/Scripts/Core/CameraFacing.cs
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RPG.Core
 {
     public class CameraFacing : MonoBehaviour
     {
-        private void Update() 
+        private void LateUpdate() 
         {
             transform.forward = Camera.main.transform.forward;
         }
